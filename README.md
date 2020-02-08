8a820af252a2629027b5a0ef266d400a49429400
Edgar Hovsepyan <edgarhovsepyangmail.com>
2020-02-08 15:48:28 +0400

    push ocean & clouds

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# _Ocan
+# Ocean & Cloud
+
+*This project demostrates the ability to create dynamically generated ocean and cloud using ThreeJS and WebGL*
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Installation
+
+npm install
+
+#### How to run
+
+npm start
+
+*After this command the application will run on localhost:8080*
---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+const exphbs = require('express-handlebars');
+
+
+var app = express()
+var http = require('http').createServer(app);
+
+app.use(express.static('build'));
+app.use(express.static('textures'));
+app.use(express.static('style'));
+
+
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+function ignoreFavicon(req, res, next) {
+  if (req.originalUrl === '/favicon.ico') {
+    res.status(204).json({nope: true});
+  } else {
+    next();
+  }
+}
+
+app.use(ignoreFavicon);
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
